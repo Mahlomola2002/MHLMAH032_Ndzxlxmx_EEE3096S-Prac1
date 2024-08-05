6aad0cf99df38f8b6d28cd9d1876ce086196f511
mahlomola <mohlomimahlomola@gmail.com>
2024-08-05 18:46:50 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,479 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdint.h>
+#include "stm32f0xx.h"
+#include <stm32f051x8.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim16;
+
+/* USER CODE BEGIN PV */
+// TODO: Define input variables
+uint32_t timer_period = 1000;
+uint8_t bitpattern1 = 0b11101001;
+
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM16_Init(void);
+/* USER CODE BEGIN PFP */
+void TIM16_IRQHandler(void);
+void setPattern(uint8_t bitpattern);
+void pattern1(void);
+void pattern2(void);
+void pattern3(void);
+void pattern4(void);
+void pattern5(void);
+void pattern6(void);
+void pattern7(void);
+void pattern8(void);
+void pattern9(void);
+double time;
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM16_Init();
+
+  /* USER CODE BEGIN 2 */
+
+  // TODO: Start timer TIM16
+  HAL_TIM_Base_Start_IT(&htim16);
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+      /* USER CODE END WHILE */
+
+      /* USER CODE BEGIN 3 */
+
+      // Check if SW0 (connected to GPIOA_0) is pressed
+      if (HAL_GPIO_ReadPin(Button3_GPIO_Port, GPIO_PIN_0) == GPIO_PIN_RESET)
+      {
+          HAL_Delay(50); // Simple debounce delay
+          if (HAL_GPIO_ReadPin(Button3_GPIO_Port, GPIO_PIN_0) == GPIO_PIN_RESET)
+          {
+              // SW0 is pressed
+              timer_period = 500; // Set delay to 0.5 seconds
+              __HAL_TIM_SET_AUTORELOAD(&htim16, timer_period - 1);
+          }
+      }
+
+      // Check if SW1 (connected to GPIOA_1) is pressed
+      else if (HAL_GPIO_ReadPin(Button3_GPIO_Port, GPIO_PIN_1) == GPIO_PIN_RESET)
+      {
+          HAL_Delay(50); // Simple debounce delay
+          if (HAL_GPIO_ReadPin(Button3_GPIO_Port, GPIO_PIN_1) == GPIO_PIN_RESET)
+          {
+              // SW1 is pressed
+              timer_period = 2000; // Set delay to 2 seconds
+              __HAL_TIM_SET_AUTORELOAD(&htim16, timer_period - 1);
+          }
+      }
+
+      // Check if SW2 (connected to GPIOA_2) is pressed
+      else if (HAL_GPIO_ReadPin(Button3_GPIO_Port, GPIO_PIN_2) == GPIO_PIN_RESET)
+      {
+          HAL_Delay(50); // Simple debounce delay
+          if (HAL_GPIO_ReadPin(Button3_GPIO_Port, GPIO_PIN_2) == GPIO_PIN_RESET)
+          {
+              // SW2 is pressed
+              timer_period = 1000; // Set delay to 1 second
+              __HAL_TIM_SET_AUTORELOAD(&htim16, timer_period - 1);
+          }
+      }
+
+      // Check if SW3 (connected to GPIOA_3) is pressed
+      else if (HAL_GPIO_ReadPin(Button3_GPIO_Port, GPIO_PIN_3) == GPIO_PIN_RESET)
+      {
+          HAL_Delay(50); // Simple debounce delay
+          if (HAL_GPIO_ReadPin(Button3_GPIO_Port, GPIO_PIN_3) == GPIO_PIN_RESET)
+          {
+              // SW3 is pressed
+              pattern1();
+          }
+      }
+  }
+  /* USER CODE END 3 */
+}
+void setPattern(uint8_t bitpattern) {
+    for (int i = 0; i < 8; i++) {
+        GPIO_PinState pinState = (bitpattern & (1 << i)) ? GPIO_PIN_SET : GPIO_PIN_RESET;
+        HAL_GPIO_WritePin(LED0_GPIO_Port, GPIO_PIN_0 << i, pinState);
+    }
+}
+
+void pattern1(void) {
+    uint8_t bitpattern = 0b11101001;
+    setPattern(bitpattern);
+}
+
+void pattern2(void) {
+    uint8_t bitpattern = 0b11010010;
+    setPattern(bitpattern);
+}
+
+void pattern3(void) {
+    uint8_t bitpattern = 0b10100100;
+    setPattern(bitpattern);
+}
+
+void pattern4(void) {
+    uint8_t bitpattern = 0b01001000;
+    setPattern(bitpattern);
+}
+
+void pattern5(void) {
+    uint8_t bitpattern = 0b10010000;
+    setPattern(bitpattern);
+}
+
+void pattern6(void) {
+    uint8_t bitpattern = 0b00100000;
+    setPattern(bitpattern);
+}
+
+void pattern7(void) {
+    uint8_t bitpattern = 0b01000000;
+    setPattern(bitpattern);
+}
+void pattern8(void) {
+    uint8_t bitpattern = 0b1000000;
+    setPattern(bitpattern);
+}
+void pattern9(void) {
+    uint8_t bitpattern = 0b00000000;
+    setPattern(bitpattern);
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  LL_FLASH_SetLatency(LL_FLASH_LATENCY_0);
+  while(LL_FLASH_GetLatency() != LL_FLASH_LATENCY_0)
+  {
+  }
+  LL_RCC_HSI_Enable();
+
+   /* Wait till HSI is ready */
+  while(LL_RCC_HSI_IsReady() != 1)
+  {
+
+  }
+  LL_RCC_HSI_SetCalibTrimming(16);
+  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_HSI);
+
+   /* Wait till System clock is ready */
+  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_HSI)
+  {
+
+  }
+  LL_SetSystemCoreClock(8000000);
+
+   /* Update the time base */
+  if (HAL_InitTick (TICK_INT_PRIORITY) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief TIM16 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM16_Init(void)
+{
+
+  /* USER CODE BEGIN TIM16_Init 0 */
+
+  /* USER CODE END TIM16_Init 0 */
+
+  /* USER CODE BEGIN TIM16_Init 1 */
+
+  /* USER CODE END TIM16_Init 1 */
+  htim16.Instance = TIM16;
+  htim16.Init.Prescaler = 8000-1;
+  htim16.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim16.Init.Period =  timer_period - 1;
+  htim16.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim16.Init.RepetitionCounter = 0;
+  htim16.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  if (HAL_TIM_Base_Init(&htim16) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM16_Init 2 */
+  NVIC_EnableIRQ(TIM16_IRQn);
+  /* USER CODE END TIM16_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOF);
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
+
+  /**/
+  LL_GPIO_ResetOutputPin(LED0_GPIO_Port, LED0_Pin);
+
+  /**/
+  LL_GPIO_ResetOutputPin(LED1_GPIO_Port, LED1_Pin);
+
+  /**/
+  LL_GPIO_ResetOutputPin(LED2_GPIO_Port, LED2_Pin);
+
+  /**/
+  LL_GPIO_ResetOutputPin(LED3_GPIO_Port, LED3_Pin);
+
+  /**/
+  LL_GPIO_ResetOutputPin(LED4_GPIO_Port, LED4_Pin);
+
+  /**/
+  LL_GPIO_ResetOutputPin(LED5_GPIO_Port, LED5_Pin);
+
+  /**/
+  LL_GPIO_ResetOutputPin(LED6_GPIO_Port, LED6_Pin);
+
+  /**/
+  LL_GPIO_ResetOutputPin(LED7_GPIO_Port, LED7_Pin);
+
+  /**/
+  GPIO_InitStruct.Pin = Button0_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
+  LL_GPIO_Init(Button0_GPIO_Port, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = Button1_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
+  LL_GPIO_Init(Button1_GPIO_Port, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = Button2_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
+  LL_GPIO_Init(Button2_GPIO_Port, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = Button3_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
+  LL_GPIO_Init(Button3_GPIO_Port, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = LED0_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(LED0_GPIO_Port, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = LED1_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(LED1_GPIO_Port, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = LED2_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(LED2_GPIO_Port, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = LED3_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(LED3_GPIO_Port, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = LED4_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(LED4_GPIO_Port, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = LED5_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(LED5_GPIO_Port, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = LED6_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(LED6_GPIO_Port, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = LED7_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(LED7_GPIO_Port, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_9;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+// Timer rolled over
+void TIM16_IRQHandler(void)
+{
+    // Acknowledge interrupt
+    HAL_TIM_IRQHandler(&htim16);
+
+    static uint8_t current_pattern = 0;
+
+    // Array of function pointers to pattern functions
+    void (*patterns[])(void) = {pattern1, pattern2, pattern3, pattern4,
+                                pattern5, pattern6, pattern7, pattern8, pattern9};
+
+    // Call the current pattern function
+    patterns[current_pattern]();
+
+    // Move to next pattern
+    current_pattern = (current_pattern + 1) % 9;
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
